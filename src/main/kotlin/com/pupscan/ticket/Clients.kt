b039694bc1ebe1e7d229b4a9e1c5406b09b445fa
Thibaut Mottet <thibaut.mottet@gmail.com>
2017-07-29 18:07:30 +0200

    Add Client controller

---
+++ src/main/kotlin/com/pupscan/ticket/Clients.kt
@@ -0,0 +1,61 @@
+package com.pupscan.ticket
+
+import org.springframework.data.repository.Repository
+import org.springframework.stereotype.Service
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/clients")
+class ClientsController(val clientsService: ClientsService) {
+
+    @RequestMapping("/all")
+    fun all() = clientsService.clients()
+
+    @RequestMapping("/client/{mail:.+}")
+    fun client(@PathVariable mail: String = "") = clientsService.client(mail)
+
+    @RequestMapping("/client/activities/{mail:.+}")
+    fun activities(@PathVariable mail: String = "") = clientsService.activities(mail)
+}
+
+@Service
+class ClientsService(val repository: ClientsRepository) {
+
+    fun clients() = repository.findAll()
+            .filter { it.mail.isNotEmpty() }
+            .map { SimpleClient(it.name.cleanName(), it.mail, it.status.capitalize()) }.toSet()
+
+    fun client(mail: String) = repository.findAllByMail(mail).map { Client(it.name, it.mail, it.tags) }
+
+    fun activities(mail: String) = activities(repository.findAllByMail(mail))
+
+    private fun activities(tickets: List<Ticket>) = tickets.map(transform = this::createActivity).toList()
+
+    private fun createActivity(ticket: Ticket): Activity {
+        if (ticket.messageSubject.contains("[Indiegogo] New contribution")) {
+            return Activity("purchase", "Indiegogo", ticket.createdDate.toddMM_hhmm(), ticket.zenDeskId)
+        } else if (ticket.messageSubject.contains("[Indiegogo] Order Refunded")) {
+            return Activity("refund", "Indiegogo", ticket.createdDate.toddMM_hhmm(), ticket.zenDeskId)
+        } else if (ticket.messageSubject.contains("[Indiegogo] New Message")) {
+            return Activity("message", "Indiegogo", ticket.createdDate.toddMM_hhmm(), ticket.zenDeskId)
+        } else if (ticket.messageSubject.contains("[Indiegogo]")) {
+            return Activity("other", "Indiegogo", ticket.createdDate.toddMM_hhmm(), ticket.zenDeskId)
+        }
+        return Activity("message", "", ticket.createdDate.toddMM_hhmm(), ticket.zenDeskId)
+    }
+}
+
+
+data class Activity(val type: String, val description: String, val date: String, val zenDeskId: Long)
+data class SimpleClient(val name: String, val email: String, val status: String)
+data class Client(val name: String, val email: String, val tags: List<String>)
+
+
+interface ClientsRepository : Repository<Ticket, String> {
+    fun findAll(): List<Ticket>
+    fun findAllByMail(email: String): List<Ticket>
+}
+
+fun String.cleanName() = this.replace("\"", "").replace("\\", "").capitalize()
--- src/main/kotlin/com/pupscan/ticket/TicketApplication.kt
+++ src/main/kotlin/com/pupscan/ticket/TicketApplication.kt
@@ -119,8 +119,8 @@ class TicketController(val repository: TicketRepository) {
                 SimpleTicket(
                         it.zenDeskId,
                         it.status,
-                        it.createdDate.format(DateTimeFormatter.ofPattern("dd/MM hh:mm")),
-                        it.updatedDate.format(DateTimeFormatter.ofPattern("dd/MM hh:mm")),
+                        it.createdDate.toddMM_hhmm(),
+                        it.updatedDate.toddMM_hhmm(),
                         it.tags.joinToString(),
                         it.name,
                         it.mail,
@@ -140,8 +140,8 @@ class TicketController(val repository: TicketRepository) {
                     SimpleTicket(
                             it.zenDeskId,
                             it.status,
-                            it.createdDate.format(DateTimeFormatter.ofPattern("dd/MM hh:mm")),
-                            it.updatedDate.format(DateTimeFormatter.ofPattern("dd/MM hh:mm")),
+                            it.createdDate.toddMM_hhmm(),
+                            it.updatedDate.toddMM_hhmm(),
                             it.tags.joinToString(),
                             it.name,
                             it.mail,
@@ -274,9 +274,16 @@ class LocalDateTimeDeserializer : JsonDeserializer<LocalDateTime>() {
 }
 
 
+
+/** Util **/
+
 fun String.escapeln() = this.replace("\n", "\\n").replace("\r", "\\n").replace("|", "")
 fun String.truncat(length: Int) = this.substring(0, if (length > this.length) this.length else length) + "..."
 fun String.safeDisplaySecret(): String {
     if (this.isBlank()) return ""
     return "X".repeat(this.length - 3) + this.substring(this.length - 3)
 }
+
+//.format(DateTimeFormatter.ofPattern("dd/MM hh:mm")
+val DDMM_HHMM = DateTimeFormatter.ofPattern("dd/MM hh:mm")
+fun LocalDateTime.toddMM_hhmm() = this.format(DDMM_HHMM)
