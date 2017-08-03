7b2beb1bbf7518261b43c2d31c9b4417235980c0
Thibaut Mottet <thibaut.mottet@gmail.com>
2017-08-04 00:27:39 +0200

    Fix pageagle

--- src/main/kotlin/com/pupscan/ticket/Application.kt
+++ src/main/kotlin/com/pupscan/ticket/Application.kt
@@ -2,28 +2,43 @@ package com.pupscan.ticket
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.context.annotation.Bean
+import org.springframework.core.Ordered
+import org.springframework.core.annotation.Order
 import org.springframework.scheduling.annotation.EnableScheduling
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer
-import org.springframework.web.servlet.config.annotation.CorsRegistry
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
+import org.springframework.stereotype.Component
+import javax.servlet.*
+import javax.servlet.http.HttpServletRequest
+import javax.servlet.http.HttpServletResponse
 
 
 @EnableScheduling
 @EnableResourceServer
 @SpringBootApplication
-class TicketApplication {
-    @Bean
-    fun corsConfigurer() =
-            object : WebMvcConfigurerAdapter() {
-                override fun addCorsMappings(registry: CorsRegistry) {
-                    registry.addMapping("/**").allowedOrigins("*")
-                }
-            }
+class TicketApplication
 
+@Component
+@Order(Ordered.HIGHEST_PRECEDENCE)
+class CorsFilter : Filter {
+    override fun doFilter(req: ServletRequest, resp: ServletResponse, chain: FilterChain) {
+        val response = resp as HttpServletResponse
+        val request = req as HttpServletRequest
+        response.setHeader("Access-Control-Allow-Origin", "*")
+        response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE")
+        response.setHeader("Access-Control-Max-Age", "3600")
+        response.setHeader("Access-Control-Allow-Headers", "x-requested-with, authorization, Content-Type, Authorization, credential, X-XSRF-TOKEN")
+
+        if ("OPTIONS".equals(request.getMethod(), ignoreCase = true)) {
+            response.status = HttpServletResponse.SC_OK
+        } else {
+            chain.doFilter(req, resp)
+        }
+    }
+
+    override fun init(p0: FilterConfig?) {}
+    override fun destroy() {}
 }
 
 fun main(args: Array<String>) {
     SpringApplication.run(TicketApplication::class.java, *args)
 }
-
